e39900ca8d8106aca172c83683dfe2cd91d17883
luka-bt <146333922+luka-bt@users.noreply.github.com>
2023-11-08 18:07:24 +0000

    Add files via upload

---
+++ Week4 Port.py
@@ -0,0 +1,8 @@
+
+a = int(input("enter number: "))
+if ( a > 100 ) : print('False, number is above')
+if ( a < 0 ) : print("False, number is below")
+if ( 0 <= a <= 100 ) : print("True")
+
+
+
---
+++ Week4.py
@@ -0,0 +1,13 @@
+from math import *
+root = sqrt(49)
+val = cos(radians(45))
+print(val)
+
+from math import pi
+print("The value of pi is", pi)
+
+msg = "hello"
+def displayTwice(msg):
+    print(msg)
+    print(msg)
+print(displayTwice(msg))
